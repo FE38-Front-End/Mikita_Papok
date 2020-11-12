04c2ae2c035ef895951bdb09eb446f566765e24d
Mikita Papok <popok11014@tut.by>
2020-11-12 13:52:42 +0300

    Исправил замечания, кроме форматирования кода. Не совсем понял, что это значит

--- lesson14/homework.js
+++ lesson14/homework.js
@@ -3,22 +3,23 @@ const data = [
     {firstName: 'Bradley', lastName: 'Pitt',age: 54},
     {firstName: 'Hannah', lastName: 'Dakota',age: 24}
   ];
-//   let b=[];
-//    for (i in users){
-//     b.push(users[i]);
 
-//  }
-//   for(i in b){
-//       b[i].firstName.toUpperCase();
-//       b[i].lastName.toUpperCase();
-      
-//   }
-//   console.log
+const b = {lastName: prompt("Введите фамилию")};
+let a ;
+for (i of data){
+   for (let key in i){
+     a = i.lastName.toUpperCase();
+     break;
+   };
+   if (b.lastName.toUpperCase() === a){     
+     break;
+   }else{     
+     continue;
+   }   
+};
+if (b.lastName.toUpperCase() === a){
+  console.log(`user name: ${i.firstName}
+     user age: ${i.age}`);}else{
+  console.log('No results found for your request');
+}
 
- 
-   let x = prompt("Введите фамилию").toUpperCase(); 
-    let a = data.find(element => element.lastName.toUpperCase()===x);
-    let b = Boolean(a);
-    if (b ===true){
-  console.log(`user name: ${a.firstName}
-  user age: ${a.age}`);}else{console.log('No results found for your request')};
