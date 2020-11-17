8788d0b8ca5801a699815f34b65fe795fd29c90c
Mikita Papok <popok11014@tut.by>
2020-11-17 16:34:00 +0300

    добавлено дз 15(не до конца)

---
+++ lesson15/lesson15.js
@@ -0,0 +1,35 @@
+
+//zadanie1
+const salaries = {John: 100, Ann: 160, Pete : 130}
+
+let total = Object.values(salaries);
+const totalValue = total.reduce((acc,q) =>{
+    console.log(acc);
+    return(acc += q);}, 0);
+console.log(totalValue);
+
+//zadanie 2
+let dom=[];
+const pod=Number(prompt('Введите кол-во подъездов'));
+const etaz= Number(prompt('введите кол-во этажей'));
+const apart=Number(prompt('введите кол-во квартир на этаже'));
+let obshee = (etaz * apart)*pod;
+
+let apartaments=[];
+let i=1;
+for (i; i<=obshee; i++){
+    apartaments.push(i);
+    
+}
+let nomeraKvartir=[];
+for(let a=1; a<=pod; a++){
+    let dannie={'podiezd': a};
+      for(let b=1; b<=etaz;b++){
+            nomeraKvartir.push(apartaments.slice(0,apart));
+            apartaments.splice(0,apart);  
+          dannie[`etazh ${b}`] = nomeraKvartir;
+          nomeraKvartir=[];                  
+      }
+    dom.push(dannie);
+}
+console.log(dom);
