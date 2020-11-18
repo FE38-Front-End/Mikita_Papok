a8911ac32e7618406727ad4f5a4a93af5c9d942e
Mikita Papok <popok11014@tut.by>
2020-11-18 23:30:32 +0300

    Ладно. Я переделал, все работает по условию. Но вопросики остались

--- lesson16/zadanie3.js
+++ lesson16/zadanie3.js
@@ -1,17 +1,16 @@
 var c = [0];
-var sequence = function (a, b) {
+function sequence(a, b) {
     let x = parseInt(c);
     if (x < a) {
         x = a;
     }
-    x += b;
-    c.splice(0, 1, x);
-    return c;
+    return function () {
+        x += b;
+        c.splice(0, 1, x);
+        return c;
+    }
 }
-// var generator=sequence(10,3);
-// alert(generator());
-// alert(generator());
-
-alert(sequence(10,3));
-alert(sequence(10,3));
-
+var generator = sequence(10, 3);
+alert(generator());
+alert(generator());
+alert(generator());
