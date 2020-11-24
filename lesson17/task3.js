19d9300a46e9cd874d1670ef285457c1d9512da4
Mikita Papok <popok11014@tut.by>
2020-11-24 12:51:57 +0300

    Я не понял, что ты имел ввиду, но переделал, на всякий

--- lesson17/task3.js
+++ lesson17/task3.js
@@ -1,22 +1,25 @@
-let a=[];
-let b=[];
-let c=[];
-for(let i=0; i<=9;i++){
-    a.push(Math.floor(Math.random()*(100-0+1)+0));
+let a = [];
+let b = [];
+let c = [];
+const randArr2 = function (min, max) {
+    for (let i = 0; i <= 9; i++) {
+        a.push(Math.floor(Math.random() * (max - min + 1) + min));
+    }
 }
-a.forEach(Element=>{
-        Element=Element.toString();
-         Element=Element.replace('0','zero');
-        b.push(Element);
+randArr2(0, 100);
+
+a.forEach(Element => {
+    Element = Element.toString();
+    Element = Element.replace('0', 'zero');
+    b.push(Element);
 })
-b.forEach(Element=>{
-    Element=Element.toString();
-     Element=Element.replace('0','zero');
-     if(!Element.includes('zero')){
-            Element=parseInt(Element);
-     }
+b.forEach(Element => {
+    Element = Element.toString();
+    Element = Element.replace('0', 'zero');
+    if (!Element.includes('zero')) {
+        Element = parseInt(Element);
+    }
     c.push(Element);
 
 })
-
 console.log(c);
