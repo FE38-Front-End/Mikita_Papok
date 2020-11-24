2ee2bf3639d5860c5a1eecbf894a4d1d181ad4f2
Mikita Papok <popok11014@tut.by>
2020-11-24 20:33:45 +0300

    переделал задание

--- feature/task1.js
+++ feature/task1.js
@@ -1,25 +1,9 @@
-const conten = function (len, win, dp, diamTrub) {
-    let p = len * win;
-    if (diamTrub < p) {
-
-        return function () {
-            let metrTrub;
-            let countMetr = 0;
-            while (p > diamTrub) {
-                metrTrub = dp;
-                p -= dp;
-                countMetr += metrTrub;
-            }
-            return countMetr;
-        }
-    } else {
-        return console.log('не поместится');
-    }
-}
 const a = Number(prompt('visota'));
 const b = Number(prompt('shirina'));
 const c = Number(prompt('glubina'));
 const d = Number(prompt('diametr'));
+var ploshKont = a * b;
+var diamTrub = (Math.PI * (d ** 2)) / 4;
+var countTrub = (Math.floor(ploshKont / diamTrub)) * c;
 
-var countTrub = conten(a, b, c, d);
-console.log(countTrub());
+console.log(countTrub);
