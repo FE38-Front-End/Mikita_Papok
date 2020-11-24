c4c227af39d303cf63c4eafb2ba3263e039603e3
Mikita Papok <popok11014@tut.by>
2020-11-24 21:04:30 +0300

    исправил исправленное

--- feature/task1.js
+++ feature/task1.js
@@ -2,8 +2,7 @@ const a = Number(prompt('visota'));
 const b = Number(prompt('shirina'));
 const c = Number(prompt('glubina'));
 const d = Number(prompt('diametr'));
-var ploshKont = a * b;
-var PipeSquare = Math.PI * d ** 2 / 4;
-var countPipe = Math.floor(ploshKont / diamTrub) * c;
-
+let kontSquare = a * b;
+let pipeSquare = Math.PI * d ** 2 / 4;
+let countPipe = Math.floor(kontSquare / pipeSquare) * c;
 console.log(countPipe);
