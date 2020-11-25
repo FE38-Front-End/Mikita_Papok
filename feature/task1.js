c05bb4dd97c3da6636de3948d27c26c1b36dff9a
Mikita Papok <popok11014@tut.by>
2020-11-26 02:43:05 +0300

    Edited classwork

--- feature/task1.js
+++ feature/task1.js
@@ -1,11 +1,14 @@
 const contHeight = Number(prompt('Введите высоту контейнера в метрах'));
 const contWidth = Number(prompt('Введите ширину контейнера в метрах'));
 const contDeep = Number(prompt('Введите длину контейнера в метрах'));
-let pipeList=[];
+let pipeList = [];
 let count1 = 0;
 let pipeDiam = 0;
 let finalCapacity = 0;
-let shortMess = {'В контейнер помещается':{}};
+let shortMess = {
+        'В контейнер помещается': {}
+};
+let lotPipe = [];
 while (true) {
         pipeDiam = Number(prompt('введите диаметр трубы в метрах'));
         if (Boolean(pipeDiam) === true) {
@@ -23,6 +26,7 @@ const kontCapacity = function (capacity) {
         initialCapacity = capacity;
         return function (pipeDiam) {
                 let pipeSquare = Math.PI * pipeDiam ** 2 / 4;
+                lotPipe.push(Math.floor(initialCapacity / pipeSquare));
                 return Math.floor(initialCapacity / pipeSquare) * contDeep;
         };
 }
@@ -34,63 +38,10 @@ for (let countDiam = 0; countDiam < pipeList.length; countDiam++) {
                 shortMess['В контейнер помещается'][`труба диаметром ${pipeList[countDiam]} м.`] = `${finalCapacity} метров`;
         } else {
                 capacity = Math.PI * pipeList[countDiam - 1] ** 2 / 4;
-                finalCapacity = kontCapacity(capacity)(pipeList[countDiam]);
+
+                finalCapacity = kontCapacity(capacity)(pipeList[countDiam]) * lotPipe[countDiam - 1];
                 shortMess['В контейнер помещается'][`труба диаметром ${pipeList[countDiam]} м.`] = `${finalCapacity} метров`;
         }
 }
 
 console.log(shortMess);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const contHeight = Number(prompt('visota'));
-// const contWidth = Number(prompt('shirina'));
-// const contDeep = Number(prompt('glubina'));
-// let pipeList = [];
-// let count1 = 0;
-// let d = 0;
-// while (true) {
-//     d = Number(prompt('введите диаметр трубы'));
-//     if (Boolean(d) === true) {
-//         count1 = Math.PI * d ** 2 / 4;
-//         count1 = Number(count1);
-//         pipeList.push(count1);
-//     } else {
-//         break;
-//     }
-// }
-// pipeList.sort((a, b) => a - b);
-// pipeList = pipeList.reverse();
-// // console.log(pipeList);
-
-// for(let i=0; i<pipeList.length;i++){
-//     if (i=0){
-
-//     }
-// }
