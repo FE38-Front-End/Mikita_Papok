276406460f6490566e99e7f395c04ee1df5da74c
Mikita Papok <popok11014@tut.by>
2020-12-14 13:57:23 +0300

    edited lesson20

--- lesson20/task1.js
+++ lesson20/task1.js
@@ -1,30 +1,31 @@
 const buttonUl = document.getElementById('append-ul-button');
 const buttonLi = document.getElementById('append-li-button');
 const buttonRemove = document.getElementById('remove-last-child-button');
-var date = new Date();
-const createList = document.createElement('ul');
+const date = new Date();
+const list = document.createElement('ul');
+const li = 'list';
 
 buttonUl.addEventListener('click', event => {
-
-    createList.textContent = 'Это список';
-    createList.classList.add('list');
-
-    return document.body.appendChild(createList);
+    list.textContent = 'Это список';
+    list.classList.add(li);
+    document.body.appendChild(list);
 });
 
 buttonLi.addEventListener('click', event => {
-    const createListElement = document.createElement('li');
-    createListElement.textContent = `${date.getHours()}: ${date.getMinutes()}`;
-    if (document.getElementsByClassName('list').length > 0) {
-        return createList.appendChild(createListElement);
+    const listElement = document.createElement('li');
+    const date = new Date();
+    listElement.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+
+    if (document.getElementsByClassName(li).length > 0) {
+        return list.appendChild(listElement);
     } else {
         return alert('списка не существует');
     }
 })
 
 buttonRemove.addEventListener('click', event => {
-    if (document.getElementsByClassName('list').length > 0 && createList.children.length > 0) {
-        return createList.removeChild(createList.lastElementChild);
+    if (document.getElementsByClassName(li).length > 0 && list.children.length > 0) {
+        return list.removeChild(list.lastElementChild);
     } else {
         return alert('нечего удалять')
 
