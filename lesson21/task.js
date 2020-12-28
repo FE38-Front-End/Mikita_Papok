3e1bea027995e71510ae9bdb9add1e70c149001c
mikitapapok <popok11014@gmail.com>
2020-12-28 10:15:33 +0300

    editted lesson21

--- lesson21/task.js
+++ lesson21/task.js
@@ -1,137 +1,133 @@
-const inputSide = document.getElementsByClassName('input-side')[0];
-const sideInput = document.getElementById('side-button');
+const sideValue = document.getElementsByClassName('input-side')[0];
+const submitButtonSide = document.getElementById('side-button');
 let diam = 0;
 let side = 0;
 let step = 0;
 let initialX = [0];
 let initialY = [0];
 const circle = document.getElementsByClassName('circle')[0];
-const inputDiam = document.getElementsByClassName('input-diametr')[0];
-const diamInput = document.getElementById('diametr-button');
-const inputStep = document.getElementsByClassName('input-step')[0];
-const stepInput = document.getElementById('step-button');
-const up = document.getElementById('up');
-const down = document.getElementById('down');
-const left = document.getElementById('left');
-const right = document.getElementById('right');
-const VALUE_KEY = 'value';
+const diamValue = document.getElementsByClassName('input-diametr')[0];
+const diamSubmitButton = document.getElementById('diametr-button');
+const stepValue = document.getElementsByClassName('input-step')[0];
+const stepSubmitButton = document.getElementById('step-button');
+const up = document.getElementsByClassName('navigation-button__up');
+const down = document.getElementsByClassName('navigation-button__down');
+const left = document.getElementsByClassName('navigation-button__left');
+const right = document.getElementsByClassName('navigation-button__right');
 
 function getValue(name) {
     return localStorage.getItem(name) || '';
 }
 
-inputSide.addEventListener("change", event => {
-    localStorage.setItem('inputSide', inputSide.value);
+sideValue.addEventListener("change", event => {
+    localStorage.setItem('sideValue', sideValue.value);
 });
-inputSide.value = getValue('inputSide');
+sideValue.value = getValue('sideValue');
 
-inputDiam.addEventListener("change",event=>{
-    localStorage.setItem('inputDiam',inputDiam.value);
+diamValue.addEventListener("change", event => {
+    localStorage.setItem('diamValue', diamValue.value);
 
 })
-inputDiam.value=getValue('inputDiam');
+diamValue.value = getValue('diamValue');
 
-inputStep.addEventListener("change",event=>{
-    localStorage.setItem('inputStep',inputStep.value);
+stepValue.addEventListener("change", event => {
+    localStorage.setItem('inputStep', stepValue.value);
 })
-inputStep.value=getValue('inputStep');
+stepValue.value = getValue('inputStep');
 
-
-sideInput.addEventListener('click', event => {
-    side = Number(inputSide.value);
+submitButtonSide.addEventListener('click', event => {
+    side = Number(sideValue.value);
     const square = document.getElementsByClassName('border')[0];
+    square.classList.add('border-show');
     square.getAttribute('style');
     square.style.width = ` ${side}px`;
     square.style.height = ` ${side}px`;
-    square.style.border = '1px solid black';
-
 })
 
-diamInput.addEventListener('click', event => {
-    diam = Number(inputDiam.value);
+diamSubmitButton.addEventListener('click', event => {
+    diam = Number(diamValue.value);
     diam = diam / 2;
     if (diam < side) {
-        // const circle = document.getElementsByClassName('circle')[0];
+        circle.classList.add('circle-show');
         circle.getAttribute('style');
         circle.style.width = `${diam}px`;
         circle.style.height = `${diam}px`;
-        circle.style.border = `1px solid black`;
-        circle.style.borderRadius = '100%';
-        circle.style.transform = `translate(0)`;
     } else {
         alert('Размер круга не должен превышать размер квадрата')
     };
 })
 
 
-stepInput.addEventListener('click', event => {
-    step = Number(inputStep.value);
+stepSubmitButton.addEventListener('click', event => {
+    step = Number(stepValue.value);
 })
+const block = document.querySelector('.input-block');
 
-up.addEventListener('click', event => {
-    down.disabled = false;
-    let fringle = ((side / 2) - (diam / 2)) * (-1);
-    initialY[0] -= step;
-    if (initialY > fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-    } else if (initialY[0] < fringle) {
-        initialY[0] = fringle;
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        up.disabled = true;
-    } else if (initialY[0] === fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        up.disabled = true;
-    }
-})
+block.addEventListener('click', e => {
+    const a = e.target.dataset.type;
+    const fringleMinus = (side / 2 - diam / 2) * (-1);
+    const fringle = side / 2 - diam / 2;
 
-down.addEventListener('click', event => {
-    up.disabled = false;
-    let fringle = ((side / 2) - (diam / 2));
-    initialY[0] += step;;
-    if (initialY[0] < fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        down.disabled = false;
-    } else if (initialY[0] > fringle) {
-        initialY[0] = fringle;
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        down.disabled = true;
-    } else if (initialY[0] === fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        down.disabled = true;
-    }
-})
-
-left.addEventListener('click', event => {
-    right.disabled = false;
-    let fringle = ((side / 2) - (diam / 2)) * (-1);
-    initialX[0] -= step;
-    if (initialX > fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-    } else if (initialX[0] < fringle) {
-        initialX[0] = fringle;
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        left.disabled = true;
-    } else if (initialX[0] === fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        left.disabled = true;
-    }
-})
-
-right.addEventListener('click', event => {
-    left.disabled = false;
-    let fringle = ((side / 2) - (diam / 2));
-    let coordY = initialX[0];
-    coordY += step;
-    initialX[0] = coordY;
-    if (initialX[0] < fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        down.disabled = false;
-    } else if (initialX[0] > fringle) {
-        initialX[0] = fringle;
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        down.disabled = true;
-    } else if (initialX[0] === fringle) {
-        circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
-        right.disabled = true;
+    switch (a) {
+        case 'up':
+            down.disabled = false;
+            initialY[0] -= step;
+            if (initialY > fringleMinus) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+            } else if (initialY[0] < fringleMinus) {
+                initialY[0] = fringleMinus;
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                up.disabled = true;
+            } else if (initialY[0] === fringleMinus) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                up.disabled = true;
+            }
+            break;
+        case 'down':
+            up.disabled = false;
+            initialY[0] += step;
+            if (initialY[0] < fringle) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                down.disabled = false;
+            } else if (initialY[0] > fringle) {
+                initialY[0] = fringle;
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                down.disabled = true;
+            } else if (initialY[0] === fringle) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                down.disabled = true;
+            }
+            break;
+        case 'left':
+            right.disabled = false;
+            initialX[0] -= step;
+            if (initialX > fringleMinus) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+            } else if (initialX[0] < fringleMinus) {
+                initialX[0] = fringleMinus;
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                left.disabled = true;
+            } else if (initialX[0] === fringleMinus) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                left.disabled = true;
+            }
+            break;
+        case 'right':
+            left.disabled = false;
+            let coordY = initialX[0];
+            coordY += step;
+            initialX[0] = coordY;
+            if (initialX[0] < fringle) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                down.disabled = false;
+            } else if (initialX[0] > fringle) {
+                initialX[0] = fringle;
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                down.disabled = true;
+            } else if (initialX[0] === fringle) {
+                circle.style.transform = `translate(${initialX[0]}px,${initialY[0]}px)`;
+                right.disabled = true;
+            }
+            break;
     }
 })
