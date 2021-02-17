c76aa2692a90dcb7a3d91ab6d7c9fd1dc978f18c
mikitapapok <popok11014@gmail.com>
2021-02-17 23:02:14 +0300

    useless dependencies were deleted. Added  pages

--- np/webpack.config.js
+++ np/webpack.config.js
@@ -15,10 +15,29 @@ module.exports = {
     devServer: {
         port: 4200
     },
+
     plugins: [
         new HTMLWebpackConfig({
-            template: './src/index.html'
+            filename: 'index.html',
+            template: './src/index.html',
+
         }),
+        new HTMLWebpackConfig({
+            filename: 'second-page.html',
+            template: './src/second-page.html',
+
+        }),
+        new HTMLWebpackConfig({
+            filename: 'third-page.html',
+            template: './src/third-page.html',
+
+        }),
+        new HTMLWebpackConfig({
+            filename: 'fourth-page.html',
+            template: './src/fourth-page.html',
+
+        }),
+
         new CleanWebpackPlugin()
     ],
     module: {
